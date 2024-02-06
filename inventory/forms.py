e330d78040fd8d7a89cd69ff13a4fda0fdd17aa6
Brendon Marques <4thmarques@gmail.com>
2024-02-06 01:11:06 -0700

    Fixed functionalities and inproved visuals

--- inventory/forms.py
+++ inventory/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class ItemForm(forms.Form):
     name = forms.CharField(max_length=100)
     quantity = forms.IntegerField(min_value=0)
+
+class AccessForm(forms.Form):
+    user = forms.CharField(max_length=100)
+    code = forms.IntegerField(max_value=999999)
--- inventory/models.py
+++ inventory/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+class Sessions(models.Model):
+    user_code = models.CharField(max_length=20, unique=True)
+    user = models.CharField(max_length=20, unique=True)
+    def __str__(self):
+        return self.user_code
+
 class Item(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=25)
     quantity = models.IntegerField()
+    user_code = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
